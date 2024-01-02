00dc30a572d842e42486bd12aa0093b6f36d8e6e
xJSRL <sorealbueno@gmail.com>
2024-01-02 15:10:55 +0800

    added app design and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AfterViewChecked, Component, ViewChild, inject } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { Observable, map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'xjsrl.github.io';
+export class AppComponent implements AfterViewChecked {
+  ngAfterViewChecked(): void {
+    this.addFadeInAnimation();
+  }
+
+  @ViewChild('drawer') drawer!: MatDrawer; 
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+   toogleDarkTheme(){
+    document.body.classList.toggle('dark-theme');
+   }
+
+   private addFadeInAnimation() {
+    const contentElement = document.querySelector('.fade-in');
+    if (contentElement) {
+      contentElement.classList.add('fade-in');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MaterialModule } from './material.module';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { BackgroundAnimationComponent } from './components/background-animation/background-animation.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SidenavComponent,
+    BackgroundAnimationComponent,
+    HomeComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]

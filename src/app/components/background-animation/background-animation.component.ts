075b3953db97f164ab71d20375f720cb6756a98a
xJSRL <sorealbueno@gmail.com>
2024-02-01 00:07:44 +0800

    added proj images and anim

--- src/app/components/background-animation/background-animation.component.ts
+++ src/app/components/background-animation/background-animation.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-background-animation',
   templateUrl: './background-animation.component.html',
   styleUrls: ['./background-animation.component.scss']
 })
-export class BackgroundAnimationComponent implements OnChanges {
+export class BackgroundAnimationComponent implements OnInit, OnChanges {
   @Input() theme: string = '';
+  routerName!: string;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.routerName = this.router.url.substring(1);
+        console.log(this.routerName); 
+      });
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['theme']) {
